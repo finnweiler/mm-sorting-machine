20ff005839fc21217321ad1eb9f149d3cbf93daf
m-f-05 <mirijam.flues@web.de>
2021-02-19 21:02:03 +0100

    comments and small changes in sort.s

--- MMSort_mmap/final/sort.s
+++ MMSort_mmap/final/sort.s
@@ -4,8 +4,8 @@
     .data
     .balign     4
 
-setPinsMessage:
-    .asciz      "motor pins 11, 17 and 27 set\n"
+startSortMessage:
+    .asciz      "started sort"
 
     .text
 
@@ -16,26 +16,38 @@ setPinsMessage:
 sort:
     str     lr, [sp, #-4]!  @store value of lr in the stack to be able to return later 
     str     r4, [sp, #-4]!
+    
+    ldr     r0, =startSortMessage
+    bl      printf
 
+    @ starts the feeder and sets the pins for the motors an co-processor
     bl      startFeeder
     bl      setMotorPins
 
+    @ infinite loop to turn the colorwheel and sort the m&ms with the color detection and outlet
     sortFor:
         mov     r4, #1
         cmp     r4, #2
         blt     sortLoop
 
     sortLoop:
+        @ turns the color wheel 90 degrees clockwise
         mov     r0, #0
         mov     r1, #400
         bl      stepColorWheel
+
+        @ starts the color detection
         bl      colordetection
+        
+        @ moves the outlet dependent on the detected color
         bl      moveOutletToNextColor
         b       sortFor
 
+    @ stops the feeder and clears the pins for the motors and co-processor
     bl      clearMotorPins
     bl      stopFeeder
 
+    @leaves the function sort
     ldr     r4, [sp], #+4
     ldr     lr, [sp], #+4   @Pop the top of the stack and put it in lr
     bx      lr
