5a59dc5244cdb2c9db06fc5bd5c5f75989232d36
hans24o <28830219+hans24o@users.noreply.github.com>
2021-02-25 13:27:31 +0000

    added calibration test

---
+++ MMSort_mmap/test/test_calibrate_outlet/calibrateOutlet.s
@@ -0,0 +1,97 @@
+@ calibrateOutlet.s
+@ Parameters: 
+@       r10 <- GPIO register
+
+    .data
+    .balign     4
+
+rightEdge:
+    .asciz      "!! searching for right edge\n"
+
+leftEdge:
+    .asciz      "!! searching for left edge\n"
+
+center:
+    .asciz      "!! moving to center, total steps made: %d\n"
+
+    .text
+
+    .balign   4
+    .global   calibrateOutlet
+    .type     calibrateOutlet, %function
+
+calibrateOutlet:
+    str     lr, [sp, #-4]!  @ store value of lr in the stack to be able to return later 
+    str     r4, [sp, #-4]!  @ store value of r4 in the stack
+
+    mov     r4, #0 @ clear r4 to use it as a counter later on
+
+    findContact:
+        mov     r0, #21 @ read pin 21
+        bl      readPin @ load value of pin 21 in r0
+
+        cmp     r0, #0 @ compare sensor value to 0
+        beq     prepareRightEdge @ if hall sensor has contact, it can start searching for the right edge
+
+        @ else:
+        mov     r0, #0 @ move clockwise
+        mov     r1, #1 @ move 1 step
+        bl      stepOutlet @ make outlet move
+
+        b       findContact @ repeat the loop
+
+    prepareRightEdge:
+        ldr     r0, =rightEdge
+        bl      printf
+
+    findRightEdge:
+        mov     r0, #21 @ read pin 21
+        bl      readPin @ load value of pin 21 in r0
+
+        cmp     r0, #0  @ compare sensor value to 0
+        bne     prepareLeftEdge @ if hall sensor lost contact, prepare to find left edge
+
+        @ else:
+        mov     r0, #1 @ move counter-clockwise
+        mov     r1, #1 @ move 1 step
+        bl      stepOutlet @ make outlet move
+
+        b       findRightEdge @ repeat the loop
+
+    prepareLeftEdge: @ move outlet a bit to the right to make sure the hall sensor is avtive again
+        ldr     r0, =leftEdge
+        bl      printf
+
+        mov     r0, #0 @ move clockwise
+        mov     r1, #5 @ move 5 steps
+        bl      stepOutlet @ make outlet move
+
+        add     r4, r4, #5 @ add 5 steps to the counter
+
+    findLeftEdge:
+        mov     r0, #0
+        mov     r1, #1
+        bl      stepOutlet
+
+        add     r4, r4, #1
+
+        mov     r0, #21
+        bl      readPin
+
+        cmp     r0, #0
+        bne     findCenter @ if hall sensor lost contact
+        b       findLeftEdge
+
+    findCenter:
+        ldr     r0, =center
+        mov     r1, r4
+        bl      printf
+
+        mov     r0, #1
+        mov     r1, r4, lsr #1
+        bl      stepOutlet
+
+
+    ldr     r4, [sp], #+4
+    ldr     lr, [sp], #+4  /* Pop the top of the stack and put it in lr */
+    bx      lr
---
+++ MMSort_mmap/test/test_calibrate_outlet/stepOutlet.s
@@ -0,0 +1,82 @@
+@ stepOutlet.s
+@ Parameters: 
+@       r0  <- direction to turn the outlet in (0=clockwise, 1=counter-clockwise)
+@       r1  <- number of steps that shall be done
+@       r10 <- GPIO register
+    
+    
+    .data
+    .balign     4
+
+stepLeftMsg:
+    .asciz      "outlet stepping %d left\n"
+stepRightMsg:
+    .asciz      "outlet stepping %d right\n"
+
+    .text
+
+    .extern usleep
+
+    .balign   4
+    .global   stepOutlet
+    .type     stepOutlet, %function
+
+stepOutlet:
+    str     lr, [sp, #-4]!  @store value of lr in the stack to be able to return later 
+    str     r4, [sp, #-4]!
+
+    mov     r4, r1
+    
+    setOutletDirection:
+        cmp     r0, #1
+        beq     setDirectionCounterClockwise
+        @ set Pin 26 to low level to turn the outlet clockwise
+        setDirectionClockwise:
+            ldr     r0, =stepRightMsg
+            bl      printf 
+
+            mov     r2, #1
+            mov     r0, r2, lsl #26
+            str     r0, [r10, #40]
+
+            b       nextOutletStep
+        @ set Pin 26 to high level to turn the outlet counter-clockwise
+        setDirectionCounterClockwise:
+            ldr     r0, =stepLeftMsg
+            bl      printf
+
+            mov     r2, #1
+            mov     r0, r2, lsl #26
+            str     r0, [r10, #28]
+    
+    nextOutletStep:
+        cmp     r4, #0
+        beq     endStepOutlet
+        sub     r4, r4, #1
+
+        @ set 'Step' Pin 12 to high and then to low level to do one step with the outlet
+        @ set 'Step' Pin 12 to high level
+        mov     r2, #1
+        mov     r0, r2, lsl #12
+        str     r0, [r10, #28]
+
+        @ add short delay
+        ldr     r0, =#200000 @ sleep 50 ms
+        bl      usleep
+
+        @ set 'Step' Pin 12 to low level
+        mov     r2, #1
+        mov     r0, r2, lsl #12
+        str     r0, [r10, #40]
+
+        @ add short delay
+        ldr     r0, =#200000 @ sleep 50 ms
+        bl      usleep
+
+        b       nextOutletStep
+    
+    @leaves the function stepOutlet
+    endStepOutlet:
+        ldr     r4, [sp], #+4
+        ldr     lr, [sp], #+4  /* Pop the top of the stack and put it in lr */
+        bx      lr
