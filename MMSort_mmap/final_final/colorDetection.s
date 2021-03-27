384d563f108683f8f0489deebed2ae00a66cafe5
hans24o <28830219+hans24o@users.noreply.github.com>
2021-03-27 09:39:04 +0000

    added comments

--- MMSort_mmap/final_final/colorDetection.s
+++ MMSort_mmap/final_final/colorDetection.s
@@ -3,7 +3,7 @@
 @ Global Parameters:
 @       r10 <- GPIO register
 @ Parameters:
-@       r11 <- COLOR register
+@       none
 @ Returns:
 @       r11 -> COLOR register
 
@@ -19,11 +19,11 @@ colorDetected:
 colorDetection:
     str     lr, [sp, #-8]!
 
-    ldr     r0, [r10, #52]
-    mov     r0, r0, lsr #22
+    ldr     r0, [r10, #52]  @ load the on/off state of the GPIO registers into r0
+    mov     r0, r0, lsr #22 @ shift r0 22 to the right to have the three color bits as the least significant bits of r0
     mov     r1, #0b111
-    and     r11, r1, r0
-    @ writes input of GPIO 22, 23, 24 into r11
+    and     r11, r1, r0 @ now execute a bitwise and operation with 0b111 to only filter out the three GPIO color bits
+                        @ the value if the detected color is now stored in the color register
 
     mov     r1, r11
     ldr     r0, =colorDetected
